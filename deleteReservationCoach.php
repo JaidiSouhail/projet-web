73dab3a07a305470133fbfba3d82060a85e26d44
Jaidi_Souhail <131622748+JaidiSouhail@users.noreply.github.com>
2023-11-29 11:26:42 +0100

    Delete deleteReservationCoach.php

--- deleteReservationCoach.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../controller/ReservationCoachC.php';
-$reservationC = new ReservationCoachC();
-$reservationC->deleteReservation($_GET["idReservationCoach"]);
-header('Location:listReservationsCoach.php');
-?>
